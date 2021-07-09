916c8b359c187eb6970cdddf3370bfa9e4f3e59e
Gustavo Silva <gustagui.dev@gmail.com>
2021-07-08 22:01:45 -0300

    aula html finalizada

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-    res.send("Wellcome")
+    res.sendFile(__dirname + "/views/home.html");
 });
 
-app.get("/maintenance", (req, res) => {
-    res.sendFile(__dirname + "/views/maintenance.html")
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html");
 });
 
-app.listen(3000, () => console.log("Server running"));
+app.get("/manutencao", (req, res) => {
+    res.sendFile(__dirname + "/views/manuntencao.html");
+});
+
+app.get("/blog", (req, res) => {
+    res.sendFile(__dirname + "/views/blog.html");
+});
+
+app.listen(3000, () => console.log("Estamos online!"));
