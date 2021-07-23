afc62027b1c7e3c4ced1ef59cb92f533fe6e0d7b
Gustavo Silva <gustagui.dev@gmail.com>
2021-07-22 22:09:02 -0300

    add form and route

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/home.html");
@@ -12,12 +14,21 @@ app.get("/home", (req, res) => {
     res.sendFile(__dirname + "/views/home.html");
 });
 
-app.get("/manutencao", (req, res) => {
-    res.sendFile(__dirname + "/views/manuntencao.html");
+app.get("/contato", (req, res) => {
+    res.sendFile(__dirname + "/views/contato.html");
 });
 
 app.get("/blog", (req, res) => {
     res.sendFile(__dirname + "/views/blog.html");
+});
+
+app.get("/manutencao", (req, res) => {
+    res.sendFile(__dirname + "/views/manuntencao.html");
+});
+
+app.post('/receber-contato', (req, res) => {
+    console.log(req.body);
+    res.send("Contato recebido");
 });
 
 app.listen(3000, () => console.log("Estamos online!"));
